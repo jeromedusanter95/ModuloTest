00bfb7f70c360207fd35f94280769b76deb8d3eb
jeromedusanter95 <dusant_j@modulotech.fr>
2020-03-02 18:58:54 +0100

    Implement test with eliminated player

--- src/PokerProblem/Test.kt
+++ src/PokerProblem/Test.kt
@@ -5,8 +5,7 @@ import kotlin.test.assertEquals
 
 // TODO CAREFUL DO NOT TOUCH THE TEST AT ANY POINTS !!!
 class Test {
-
-    // Two players have each 100 dollars when the turn start. They both bet 50 dollars and they compare their cards at the end
+    // Two players have each 100 dollars when the turn starts. They both bet 50 dollars and they compare their cards at the end
     @Test
     fun test1() {
         val inputs = listOf(
@@ -44,8 +43,7 @@ class Test {
         assertEquals(outputs, Problem.solve(inputs))
     }
 
-
-    // Two players have each 100 dollars when the turn start. They both bet 50 dollars. Second player bluff and first player fold
+    // Two players have each 100 dollars when the turn starts. They both bet 50 dollars. Second player bluff and first player fold
     @Test
     fun test2() {
         val inputs = listOf(
@@ -83,7 +81,7 @@ class Test {
         assertEquals(outputs, Problem.solve(inputs))
     }
 
-    // Three players have each 100 dollars when the turn start. They all bet 50 dollars. First player and third player have the same hands. They split the main pot
+    // Three players have each 100 dollars when the turn starts. They all bet 50 dollars. First player and third player have the same hands. They split the main pot
     @Test
     fun test3() {
         val inputs = listOf(
@@ -135,4 +133,43 @@ class Test {
         )
         assertEquals(outputs, Problem.solve(inputs))
     }
+
+    // Two players have each 100 dollars when the turn starts. They all bet 100 dollars. First player won. Second Player is Eliminated
+    @Test
+    fun test4() {
+        val inputs = listOf(
+            Player(
+                id = 0,
+                stack = 0,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 1,
+                stack = 0,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 2,
+                statePlayer = StatePlayer.PLAYING
+            )
+        )
+        val outputs = listOf(
+            Player(
+                id = 0,
+                stack = 200,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 1,
+                stack = 0,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.ELIMINATED
+            )
+        )
+        assertEquals(outputs, Problem.solve(inputs))
+    }
+
 }
