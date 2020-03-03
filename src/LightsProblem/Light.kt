6c70c8103b6435e27e0836373bf4a4647f250a50
jeromedusanter95 <dusant_j@modulotech.fr>
2020-03-03 18:22:01 +0100

    Remove Light Problem

--- src/LightsProblem/Light.kt
+++
@@ -1,6 +0,0 @@
-package LightsProblem
-
-data class Light(
-    val distanceInMeter: Int,
-    val durationInSecond: Int
-)
--- src/LightsProblem/Problem.kt
+++
@@ -1,9 +0,0 @@
-package LightsProblem
-
-object Problem {
-    fun solve(speedLimit: Int, lights: List<Light>): Int {
-        //TODO implement algorithm here
-        // Return the speed in km/h
-        return 0
-    }
-}
