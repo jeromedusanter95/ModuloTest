d9c483173b0bacbd683833828c80f64ce70da505
jeromedusanter95 <dusant_j@modulotech.fr>
2020-03-03 17:15:11 +0100

    Implement Test with everything

--- src/PokerProblem/Test.kt
+++ src/PokerProblem/Test.kt
@@ -6,19 +6,20 @@ import kotlin.test.assertEquals
 // TODO CAREFUL DO NOT TOUCH THE TEST AT ANY POINTS !!!
 class Test {
     // Test hands between players
-    // Two players have each 100 dollars when the turn starts. They both bet 50 dollars and they compare their cards at the end
+    // Two players have each 100 dollars when the turn starts.
+    // They both bet 50 dollars and they compare their cards at the end
     @Test
     fun test1() {
         val inputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 2,
@@ -27,14 +28,14 @@ class Test {
         )
         val outputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 150,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
@@ -45,19 +46,20 @@ class Test {
     }
 
     // Test player folded
-    // Two players have each 100 dollars when the turn starts. They both bet 50 dollars. Second player bluff and first player fold
+    // Two players have each 100 dollars when the turn starts.
+    // They both bet 50 dollars. Second player bluff and first player fold
     @Test
     fun test2() {
         val inputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 1,
                 statePlayer = StatePlayer.FOLDED
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 2,
@@ -66,14 +68,14 @@ class Test {
         )
         val outputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 50,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 150,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
@@ -84,26 +86,27 @@ class Test {
     }
 
     // Test equality between players
-    // Three players have each 100 dollars when the turn starts. They all bet 50 dollars. First player and third player have the same hands. They split the main pot
+    // Three players have each 100 dollars when the turn starts.
+    // They all bet 50 dollars. First player and third player have the same hands. They split the main pot
     @Test
     fun test3() {
         val inputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 2,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 2,
+                id = 3,
                 stack = 50,
                 stackBetCurrentTurn = 50,
                 handCurrentTurn = 1,
@@ -112,21 +115,21 @@ class Test {
         )
         val outputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 125,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 2,
+                id = 3,
                 stack = 125,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
@@ -137,20 +140,21 @@ class Test {
         assertEquals(outputs, Problem.solve(inputs))
     }
 
-    //Test state_player Eliminated and All_in
-    // Two players have each 100 dollars when the turn starts. They all bet 100 dollars so they are ALL_IN. First player won. Second Player is Eliminated
+    // Test state_player Eliminated and All_in
+    // Two players have each 100 dollars when the turn starts.
+    // They all bet 100 dollars so they are ALL_IN. First player won. Second Player is Eliminated
     @Test
     fun test4() {
         val inputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 0,
                 stackBetCurrentTurn = 100,
                 handCurrentTurn = 1,
                 statePlayer = StatePlayer.ALL_IN
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 0,
                 stackBetCurrentTurn = 100,
                 handCurrentTurn = 2,
@@ -159,14 +163,14 @@ class Test {
         )
         val outputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 200,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 0,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
@@ -184,21 +188,21 @@ class Test {
     fun test5() {
         val inputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 0,
                 stackBetCurrentTurn = 100,
                 handCurrentTurn = 1,
                 statePlayer = StatePlayer.ALL_IN
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 150,
                 handCurrentTurn = 3,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 2,
+                id = 3,
                 stack = 150,
                 stackBetCurrentTurn = 150,
                 handCurrentTurn = 2,
@@ -207,21 +211,21 @@ class Test {
         )
         val outputs = listOf(
             Player(
-                id = 0,
+                id = 1,
                 stack = 300,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 1,
+                id = 2,
                 stack = 50,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
             ),
             Player(
-                id = 2,
+                id = 3,
                 stack = 250,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
@@ -316,7 +320,7 @@ class Test {
         assertEquals(outputs, Problem.solve(inputs))
     }
 
-    // Test people get back money with one pot if they bet too much
+    // Test people get back money if they bet most money than the current stack of the opponent with one pot
     // Two players have respectively 100 and 200 dollars when the turn starts.
     // Player two all_in, Player one all_in
     @Test
@@ -348,6 +352,176 @@ class Test {
             Player(
                 id = 2,
                 stack = 100,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            )
+        )
+        assertEquals(outputs, Problem.solve(inputs))
+    }
+
+    // Test people get back money if they bet most money than the current stack of the opponent with several pots
+    // Five players have respectively 100, 200, 300, 400, 500 dollars when the turn starts.
+    // Pre flop : Player 1 all_in, they all called
+    // Flop : Player 4 bet 200 dollars, player 2 all_in, player 3 fold, player 5 call
+    // Turn : Player 5 all_in Player 4 all_in
+    // First player has the best hand then player 2 then player 3...etc
+    @Test
+    fun test8() {
+        val inputs = listOf(
+            Player(
+                id = 1,
+                stack = 0,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 1,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 2,
+                stack = 0,
+                stackBetCurrentTurn = 200,
+                handCurrentTurn = 2,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 3,
+                stack = 200,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 3,
+                statePlayer = StatePlayer.FOLDED
+            ),
+            Player(
+                id = 4,
+                stack = 0,
+                stackBetCurrentTurn = 400,
+                handCurrentTurn = 4,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 5,
+                stack = 0,
+                stackBetCurrentTurn = 500,
+                handCurrentTurn = 5,
+                statePlayer = StatePlayer.ALL_IN
+            )
+        )
+        val outputs = listOf(
+            Player(
+                id = 1,
+                stack = 500,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 2,
+                stack = 300,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 3,
+                stack = 200,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 4,
+                stack = 400,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 5,
+                stack = 100,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            )
+        )
+        assertEquals(outputs, Problem.solve(inputs))
+    }
+
+    // Test everything together (several pots, equality, eliminated, money repaid...)
+    // Five players have respectively 100, 200, 300, 400, 500 dollars when the turn starts.
+    // Pre flop : Player 1 all_in, they all called
+    // Flop : Player 4 bet 200 dollars, player 2 all_in, player 3 fold, player 5 call
+    // Turn : Player 5 all_in Player 4 all_in
+    // player2 and player5 has the best hand then player 3 then player 4 then player one
+    @Test
+    fun test9() {
+        val inputs = listOf(
+            Player(
+                id = 1,
+                stack = 0,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 4,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 2,
+                stack = 0,
+                stackBetCurrentTurn = 200,
+                handCurrentTurn = 1,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 3,
+                stack = 200,
+                stackBetCurrentTurn = 100,
+                handCurrentTurn = 2,
+                statePlayer = StatePlayer.FOLDED
+            ),
+            Player(
+                id = 4,
+                stack = 0,
+                stackBetCurrentTurn = 400,
+                handCurrentTurn = 3,
+                statePlayer = StatePlayer.ALL_IN
+            ),
+            Player(
+                id = 5,
+                stack = 0,
+                stackBetCurrentTurn = 500,
+                handCurrentTurn = 1,
+                statePlayer = StatePlayer.ALL_IN
+            )
+        )
+        val outputs = listOf(
+            Player(
+                id = 1,
+                stack = 0,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.ELIMINATED
+            ),
+            Player(
+                id = 2,
+                stack = 400,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 3,
+                stack = 200,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.PLAYING
+            ),
+            Player(
+                id = 4,
+                stack = 0,
+                stackBetCurrentTurn = 0,
+                handCurrentTurn = -1,
+                statePlayer = StatePlayer.ELIMINATED
+            ),
+            Player(
+                id = 5,
+                stack = 900,
                 stackBetCurrentTurn = 0,
                 handCurrentTurn = -1,
                 statePlayer = StatePlayer.PLAYING
