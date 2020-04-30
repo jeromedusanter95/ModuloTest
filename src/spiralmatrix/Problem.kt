fb7d145bd9286f2e591b987655d4ce1058e50e3e
dusant_j@modulotech.fr <dusant_j@modulotech.fr>
2020-04-30 11:01:51 +0200

    Initialize problem

---
+++ src/spiralmatrix/Problem.kt
@@ -0,0 +1,8 @@
+package spiralmatrix
+
+object Problem {
+    fun solve(matrix: Array<Array<Char>>): String {
+        //TODO solve the problem here
+        return ""
+    }
+}
---
+++ src/spiralmatrix/Test.kt
@@ -0,0 +1,12 @@
+package spiralmatrix
+
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun test1() {
+        val boolean = true
+        assert(boolean)
+    }
+}
