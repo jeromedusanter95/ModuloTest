3d1d412eaef68d5ccd1a675a6cb1b6f71bc015ae
dusant_j@modulotech.fr <dusant_j@modulotech.fr>
2020-04-30 12:16:17 +0200

    Add two more test

--- src/spiralmatrix/Test.kt
+++ src/spiralmatrix/Test.kt
@@ -14,6 +14,7 @@ class Test {
      * |
      * X
      * First position is E, so firstPosX = 1, firstPosY = 1 ( E = matrix[1][1])
+     * linesNumber = 3 columnsNumber = 3
      * Answer should be : EDGHIFCBA
      **/
 
@@ -33,5 +34,67 @@ class Test {
         matrix[2][2] = 'I'
         val answer = Problem.solve(1, 1, linesNumber, columnsNumber, matrix)
         assertEquals("EDGHIFCBA", answer)
+    }
+
+    /**
+     * Matrix 3 * 3
+     * --------> Y
+     * | A B C
+     * | D E F
+     * | G H I
+     * |
+     * X
+     * First position is B, so firstPosX = 0, firstPosY = 1 ( B = matrix[0][1])
+     * linesNumber = 3 columnsNumber = 3
+     * Answer should be : BADGHIFC
+     **/
+
+    @Test
+    fun test2() {
+        val linesNumber = 3
+        val columnsNumber = 3
+        val matrix = Array(linesNumber) { Array(columnsNumber) { 'A' } }
+        matrix[0][0] = 'A'
+        matrix[0][1] = 'B'
+        matrix[0][2] = 'C'
+        matrix[1][0] = 'D'
+        matrix[1][1] = 'E'
+        matrix[1][2] = 'F'
+        matrix[2][0] = 'G'
+        matrix[2][1] = 'H'
+        matrix[2][2] = 'I'
+        val answer = Problem.solve(0, 1, linesNumber, columnsNumber, matrix)
+        assertEquals("BADGHIFC", answer)
+    }
+
+    /**
+     * Matrix 3 * 3
+     * --------> Y
+     * | A B C
+     * | D E F
+     * | G H I
+     * |
+     * X
+     * First position is I, so firstPosX = 3, firstPosY = 3 ( I = matrix[3][3])
+     * linesNumber = 3 columnsNumber = 3
+     * Answer should be : IHGDABCFE
+     **/
+
+    @Test
+    fun test3() {
+        val linesNumber = 3
+        val columnsNumber = 3
+        val matrix = Array(linesNumber) { Array(columnsNumber) { 'A' } }
+        matrix[0][0] = 'A'
+        matrix[0][1] = 'B'
+        matrix[0][2] = 'C'
+        matrix[1][0] = 'D'
+        matrix[1][1] = 'E'
+        matrix[1][2] = 'F'
+        matrix[2][0] = 'G'
+        matrix[2][1] = 'H'
+        matrix[2][2] = 'I'
+        val answer = Problem.solve(2, 2, linesNumber, columnsNumber, matrix)
+        assertEquals("IHGDABCFE", answer)
     }
 }
