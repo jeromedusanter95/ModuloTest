d2ce3849ca613e8edd0404dbb8b22990d26d1440
dusant_j@modulotech.fr <dusant_j@modulotech.fr>
2020-04-30 11:23:51 +0200

    Create first test

--- src/spiralmatrix/Problem.kt
+++ src/spiralmatrix/Problem.kt
@@ -1,7 +1,24 @@
 package spiralmatrix
 
 object Problem {
-    fun solve(matrix: Array<Array<Char>>): String {
+
+    /**
+     * Write an algorithm which allows to traverse a 2d matrix in the form of a spiral
+     * Rules :
+     * You should always goes into the same direction as the previous one if you can
+     * If you can't, you should take another direction with that order of preferences :
+     * Left, Down, Right, and Up (meaning if you can go either left or down, you have to go left)
+     * You can't pass on a position if you already pass on it
+     **/
+
+
+    fun solve(
+        firstPosX: Int,
+        firstPosY: Int,
+        linesNumber: Int,
+        columnsNumber: Int,
+        matrix: Array<Array<Char>>
+    ): String {
         //TODO solve the problem here
         return ""
     }
--- src/spiralmatrix/Test.kt
+++ src/spiralmatrix/Test.kt
@@ -1,12 +1,37 @@
 package spiralmatrix
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class Test {
 
+    /**
+     * Matrix 3 * 3
+     * --------> Y
+     * | A B C
+     * | D E F
+     * | G H I
+     * |
+     * X
+     * First position is E, so firstPosX = 1, firstPosY = 1 ( E = matrix[1][1])
+     * Answer should be : EDGHIFCBA
+     **/
+
     @Test
     fun test1() {
-        val boolean = true
-        assert(boolean)
+        val linesNumber = 3
+        val columnsNumber = 3
+        val matrix = Array(linesNumber) { Array(columnsNumber) { 'A' } }
+        matrix[0][0] = 'A'
+        matrix[0][1] = 'B'
+        matrix[0][2] = 'C'
+        matrix[1][0] = 'D'
+        matrix[1][1] = 'E'
+        matrix[1][2] = 'F'
+        matrix[2][0] = 'G'
+        matrix[2][1] = 'H'
+        matrix[2][2] = 'I'
+        val answer = Problem.solve(1, 1, linesNumber, columnsNumber, matrix)
+        assertEquals("EDGHIFCBA", answer)
     }
 }
